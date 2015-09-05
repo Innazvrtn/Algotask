e107c146e18214faa1f072afc24d3af8041d22d4
Innazvrtn <innazvrtn@gmail.com>
2015-09-05 22:39:04 +0300

    Algo
    Signed-off-by: Innazvrtn <innazvrtn@gmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TESt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 173);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "x1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "x2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "x3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "x4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "B";
+            this.Column5.Name = "Column5";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column6,
+            this.Column7});
+            this.dataGridView2.Location = new System.Drawing.Point(692, 21);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(287, 173);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "X";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "E";
+            this.Column7.Name = "Column7";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12});
+            this.dataGridView3.Location = new System.Drawing.Point(44, 200);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(545, 172);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "x1";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "x2";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "x3";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "x4";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "B";
+            this.Column12.Name = "Column12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 384);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Метод Гаусса";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESt
+{
+    public partial class Form1 : Form
+    {
+        void MassWrite(double[,] A, double[] B )
+        {
+            
+            for (int i = 0; i < 4; i++)
+            {
+                this.dataGridView1.Rows.Add();
+                for (int j = 0; j < 4; j++)
+                {
+                    this.dataGridView1.Rows[i].Cells[j].Value = (String.Format("{0:0.0}",A[i, j]));
+                }
+                this.dataGridView1.Rows[i].Cells[4].Value = (String.Format("{0:0.0}",B[i]));
+            }
+
+        }
+        void FunctinoWork()
+        {
+            int n = 3;
+            double[,] A;
+            double[] B;
+            double[] X=new double[n+1];
+            double[] Z = new double[n+1];
+            double[] E = new double[n + 1];
+            int[] ArrayCh = new int[n+1];
+
+            A = new double[4, 4] { { -3.0, 4.0, 1.0, 4.0 }, { 0, 1.0, 3.0, 2.0 }, { 4.0, 0, -2.0, -3.0 }, { 1000.0, 3.0, 1.0, -5.0 } };
+            B = new double[] { -1.0, -1.0, 4.0, -2.0 };
+            MassWrite(A, B);
+            //array change
+            for (int i = 0; i <= n; i++)
+            {
+                ArrayCh[i] = i;
+            }
+            /// Прямой проход
+            for (int i = 0; i < n; i++)
+            {
+                int maxIndx = i;
+                double maxval = A[i, i];
+
+                /// Поиск главного элемента в строке
+                for (int j = 0; j < n; j++)
+                    if (Math.Abs(maxval) < Math.Abs(A[i, j]))
+                    {
+                        maxval = A[i, j];
+                 
+                    }
+                //перестановка стопчиків
+                if (i != maxIndx)
+                {
+                    
+                    for (int k = 0; k <= n; k++)
+                    {
+                        double c;
+                        c = A[k, i];
+                        A[k, i] = A[k, maxIndx];
+                        A[k, maxIndx] = c;
+                    }
+                    int tmp = ArrayCh[i];
+                    ArrayCh[i] = ArrayCh[maxIndx];
+                    ArrayCh[maxIndx] = tmp;
+                }
+                /// Делим строчку на главный элемент
+
+                double R;
+                for (int k = i+1; k <= n; k++)
+                {
+                    R = A[k, i] / A[i,i];
+                    for (int j = i; j <=n; j++)
+                    {
+                        A[k, j] = A[k, j] - R * A[i,j];
+                    }
+                        B[k] = B[k] - B[i] * R;
+                    
+                }
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                this.dataGridView3.Rows.Add();
+                for (int j = 0; j < 4; j++)
+                {
+                    this.dataGridView3.Rows[i].Cells[j].Value = (String.Format("{0:0.0}", A[i, j]));
+                }
+                this.dataGridView3.Rows[i].Cells[4].Value = (String.Format("{0:0.0}", B[i]));
+            }
+            Z[n] = B[n] / A[n, n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                double S = 0;
+                for (int j = i+1; j <= n; j++)
+                {
+  S=S+A[i,j]*Z[j];
+   Z[i]=(B[i]-S)/A[i,i];
+   
+                }
+            }
+          
+            for (int i = 0; i<= n; i++)
+            {
+                X[ArrayCh[i]] = Z[i];
+              
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                this.dataGridView2.Rows.Add();
+                this.dataGridView2.Rows[i].Cells[0].Value = (String.Format("{0:0.00000}",X[i]));
+            }
+            
+  for (int j = 0; j <= n; j++)
+            {
+           
+      double S = 0; 
+            for (int i = 0; i <= n; i++)
+            {
+               S= S + A[j, i] * X[i];
+                
+            }
+       E[j]=S-B[j]; 
+      
+      this.dataGridView2.Rows[j].Cells[1].Value = (String.Format("{0:0.00E+00}", E[j]));
+        }
+  }
+        public Form1()
+        {
+            InitializeComponent();
+            FunctinoWork();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
